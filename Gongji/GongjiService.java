ad137d06bff7e3747e1851f0fa0d3cb7204a1b1d
SEONJAE KIM <79892278+kimseonjae333@users.noreply.github.com>
2023-08-20 22:05:23 +0900

    Add files via upload

---
+++ Gongji/GongjiService.java
@@ -0,0 +1,10 @@
+package kr.ac.kopo.ctc.kopo39.service;
+
+import kr.ac.kopo.ctc.kopo39.dto.Pagination;
+
+public interface GongjiService {
+	Pagination getPagination(int C, int countPerPage);
+	//C와 countPerPage 인자값을 기반으로 Pagination 클래스의 객체를 생성하여 반환하는 메서드
+
+	//int countVisitor(); 방문자 카운트 계산하는 변수
+}
